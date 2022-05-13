39f382f5abdbd0af5bc1761dfaa2b9dad63cf3c0
Andre <andre_arias.29@hotmail.com>
2022-05-13 14:38:59 -0300

    Commit 2 - Pagina inicial do jogo

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = ()=>{
+    return (
+    <div className="start">
+        <h1>Secret Word</h1>
+        <p>Clique para começar a jogar</p>
+        <button>Iniciar jogo</button>
+    </div>
+    );
+};
+
+export default StartScreen;
