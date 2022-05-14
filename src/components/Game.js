c3b77a90b355d334eaa9fa182e7d5c8cdaefcc10
Andre <andre_arias.29@hotmail.com>
2022-05-14 05:31:34 -0300

    Commit 6 - Usando os states no jogo

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import "./Game.css";
 
-const Game = ({verifyLetter}) => {
+const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score,}) => {
   return (
     <div className="game">
         <p className="points">
-            <span>Pontuação: 000</span>
+            <span>Pontuação: {score}</span>
         </p>
         <h1>Adivinhe a palavra:</h1>
         <h3 className="tip">
-            Dica: <span>Dica...</span>
+            Dica: <span>{pickedCategory}</span>
         </h3>
+        <p>Você ainda tem {guesses} tentativa(s).</p>
         <div className="wordContainer">
-            <span className="letter">A</span>
-            <span className="blankSquare"></span>
+            {letters.map((letter, i)=> (
+                guessedLetters.includes(letter) ? (
+                    <span key={i} className="letter">{letter}</span>
+                ) : (
+                    <span key={i} className="blankSquare"> </span>
+                )
+            ))}
         </div>
 
         <div className="letterContainer">
