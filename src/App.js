5bf0a3384a4a4da7f7e4e39fe58b1f23ccb04603
Andre <andre_arias.29@hotmail.com>
2022-05-14 04:52:51 -0300

    Commit 4 - Sorteio da categoria e palavra

--- src/App.js
+++ src/App.js
@@ -19,12 +19,47 @@ const stages = [
 ];
 
 function App() {
-  const[gameStage, setGameStage] = useState(stages[0].name);
+  const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
-  //console.log(words);
+  
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random()* Object.keys(categories).length)];
+
+    console.log(category);
+
+    //pick a random word
+    const word = words[category][Math.floor(Math.random()* words[category].length)];
+
+    console.log(word);
+
+    return{word, category}; //retorna como objeto
+  }
 
   //starts secret word game
   const startGame = () => {
+    
+    //pick word and category
+    const {word,category} = pickWordAndCategory();
+    
+    //create an array of letters
+    let wordLetters = word.split(""); //separa todas as letras
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    
+    console.log(word,category);
+    console.log(wordLetters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   }
 
