d4ac316531360aeea313c88c6a512a58a9d8fb92
Andre <andre_arias.29@hotmail.com>
2022-05-14 04:32:30 -0300

    Commit 3 - Adicionando os componentes e estagios do jogo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+//css
 import './App.css';
+
+// react
+import {useCallback, useEffect, useState} from 'react'
+
+//data
+import { wordsList } from './data/words';
+
+//components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id:1, name: "start"},
+  {id:2, name: "game"},
+  {id:3, name: "end"},
+];
 
 function App() {
+  const[gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+  //console.log(words);
+
+  //starts secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  //process letter input
+  const verifyLetter = () =>{
+    setGameStage(stages[2].name);
+  }
+
+  //restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage==='start' && <StartScreen startGame={startGame}/>}
+      {gameStage==='game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage==='end' && <GameOver retry={retry}/>}
     </div>
   );
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,12 @@
+import React from "react";
 import "./StartScreen.css";
 
-const StartScreen = ()=>{
+const StartScreen = ({startGame})=>{
     return (
     <div className="start">
         <h1>Secret Word</h1>
         <p>Clique para começar a jogar</p>
-        <button>Iniciar jogo</button>
+        <button onClick={startGame}>Iniciar jogo</button>
     </div>
     );
 };
