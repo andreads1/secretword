5352aeb08f3692101d29f308e7dbcd51331de2aa
Andre <andre_arias.29@hotmail.com>
2022-05-15 00:41:41 -0300

    Commit 7 - Inserindo letras e verificando letras certas e erradas

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useState,useRef } from 'react';
 import "./Game.css";
 
 const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score,}) => {
-  return (
+    
+    const [letter, setLetter] = useState("");
+    const letterInputRef = useRef(null);
+
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+
+        verifyLetter(letter);
+
+        setLetter("");
+
+        letterInputRef.current.focus();
+    }
+
+    return (
     <div className="game">
         <p className="points">
             <span>Pontuação: {score}</span>
@@ -24,15 +40,22 @@ const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters
 
         <div className="letterContainer">
             <p>Chute uma letra:</p>
-            <form>
-                <input type="text" name="letter" maxLength="1" required></input>
+            <form onSubmit={handleSubmit}>
+                <input type="text" 
+                name="letter" 
+                maxLength="1" 
+                required 
+                onChange={(e) => setLetter(e.target.value)} value={letter}
+                ref={letterInputRef}
+                ></input>
                 <button>Jogar!</button>
             </form>
         </div>
         <div className="wrongLettersContainer">
             <p>Letras já usadas:</p>
-            <span>a,</span>
-            <span>b,</span>
+            {wrongLetters.map((letter, i)=> (
+                <span key={i}>{letter}, </span>
+            ))}
         </div>
     </div>
   )
